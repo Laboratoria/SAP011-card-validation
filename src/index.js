fe2e18f44ae3fc730053f48d77e7fd50fbe0d6a3
Leticia Bezerra <leticia21bezerradearaujo@gmail.com>
2023-07-14 10:42:54 -0300

    fist-commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+let submit = document.getElementById("btn-submit");
+
+submit.addEventListener('click', function(e) {
+
+    e.preventDefault();
+
+    console.log("funcionou!");
+} )
