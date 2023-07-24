27d43b5094f0eee1fcf3e447b6c5a121cf20e17a
2karina <karina_kap@hotmail.com>
2023-07-24 15:28:24 -0300

    deletado paginas nomeadas com 'negado', animação cancelada

--- src/index.js
+++ src/index.js
@@ -2,3 +2,46 @@ import validator from './validator.js';
 
 console.log(validator);
 
+        // Função para aplicar a máscara ao número do cartão de crédito
+function maskify(creditCardNumber) {
+      const maskedString = '#'.repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
+      return maskedString;
+    }
+
+         // Função para mascarar o número do cartão de crédito enquanto o usuário digita
+function mascararNumeroCartao(input) {
+      const creditCardNumber = input.value.replace(/\s/g, ''); // Remover espaços em branco
+      const maskedNumber = maskify(creditCardNumber); // Aplicar a função maskify
+      input.value = maskedNumber; // Atualizar o valor do campo de entrada
+    }
+
+      //Função para validar o número do cartão de  crédito quando o usuário clicar no botão 'Validar'
+function validarCartao(event) {
+      event.preventDefault();
+
+      // Obter o valor do campo de entrada novamente
+      const creditCardNumber = numeroCartaoInput.value.replace(/\s/g, ''); // Remover espaços em branco
+      const isValid = validator.isValid(creditCardNumber);
+
+  if (isValid) {
+      alert('Número de cartão válido!');
+  } else {
+      alert('Número de cartão inválido! Por favor, verifique o número digitado e tente novamente.');
+  }
+}
+
+
+      // Obter o elemento de entrada do número do cartão pelo seu ID
+      const numeroCartaoInput = document.getElementById('numeroCartao');
+
+      
+      // Adicionar o evento oninput ao campo de entrada para chamar a função mascararNumeroCartao
+      numeroCartaoInput.addEventListener('input', function () {
+        mascararNumeroCartao(this);
+}
+);
+
+
+//Adicionar o evento onclick ao botão "Validar" para chamar a função validarCartao
+        const validarButton = document.querySelector('button[type="submit"]');
+        validarButton.addEventListener('click', validarCartao);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,34 @@
 const validator = {
-  // ...
+
+    //Função para validar o número do cartão de crédito usando o Algoritimo de Luhn
+    isValid: function (creditCardNumber) {
+  
+    //Remover espaços em branco do número do cartão
+    const cleanedNumber = creditCardNumber.replace(/\s/g, '');
+
+    //Converter o número de cartão em um array de dígitos
+    const digitsArray = Array.from(cleanedNumber).map(Number);
+
+    //Inverter o array de dígitos
+    digitsArray.reverse();
+
+    //Aplicar o Algoritimo de Luhn
+      let sum = 0;
+        for (let i = 0; i < digitsArray.length; i++) {
+          if (i % 2 !== 0) {
+            let doubleDigits = digitsArray[i] * 2;
+            if (doubleDigits > 9) {
+              doubleDigits -= 9;
+            }
+        sum += doubleDigits;
+        } else {
+        sum +=digitsArray[i];
+      }
+  }
+
+   //Verificar se o resultado da soma é um múltiplo de 10
+  return sum % 10 === 0;
+ },
 };
 
 export default validator;
