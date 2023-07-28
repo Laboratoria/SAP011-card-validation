096b7d5ab89f144325cf48fda5d6d69cf47ec4fe
2karina <karina_kap@hotmail.com>
2023-07-28 18:50:32 -0300

    Atualizado todo o CSS, aplicado uma animação no final, todos os comandos estão funcionando

--- src/index.js
+++ src/index.js
@@ -2,46 +2,54 @@ import validator from './validator.js';
 
 console.log(validator);
 
-        // Função para aplicar a máscara ao número do cartão de crédito
-function maskify(creditCardNumber) {
-      const maskedString = '#'.repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
-      return maskedString;
-    }
+// Obter o elemento de entrada do número do cartão pelo seu ID
+const numeroCartaoInput = document.getElementById('numeroCartao');
 
-         // Função para mascarar o número do cartão de crédito enquanto o usuário digita
-function mascararNumeroCartao(input) {
-      const creditCardNumber = input.value.replace(/\s/g, ''); // Remover espaços em branco
-      const maskedNumber = maskify(creditCardNumber); // Aplicar a função maskify
-      input.value = maskedNumber; // Atualizar o valor do campo de entrada
-    }
-
-      //Função para validar o número do cartão de  crédito quando o usuário clicar no botão 'Validar'
-function validarCartao(event) {
-      event.preventDefault();
-
-      // Obter o valor do campo de entrada novamente
-      const creditCardNumber = numeroCartaoInput.value.replace(/\s/g, ''); // Remover espaços em branco
-      const isValid = validator.isValid(creditCardNumber);
-
-  if (isValid) {
-      alert('Número de cartão válido!');
-  } else {
-      alert('Número de cartão inválido! Por favor, verifique o número digitado e tente novamente.');
-  }
-}
-
-
-      // Obter o elemento de entrada do número do cartão pelo seu ID
-      const numeroCartaoInput = document.getElementById('numeroCartao');
-
-      
-      // Adicionar o evento oninput ao campo de entrada para chamar a função mascararNumeroCartao
-      numeroCartaoInput.addEventListener('input', function () {
-        mascararNumeroCartao(this);
-}
-);
-
+// Adicionar o evento oninput ao campo de entrada para chamar a função mascararNumeroCartao
+numeroCartaoInput.addEventListener('input', function () {
+  validator.mascararNumeroCartao(this);
+});
 
 //Adicionar o evento onclick ao botão "Validar" para chamar a função validarCartao
-        const validarButton = document.querySelector('button[type="submit"]');
-        validarButton.addEventListener('click', validarCartao);
+const validarButton = document.querySelector('button[type="submit"]');
+validarButton.addEventListener('click', function (event) {
+  validarCartao(event);
+});
+
+function validarCartao(event) {
+  event.preventDefault();
+
+  const creditCardNumber = numeroCartaoInput.value.replace(/\s/g, ''); // Remover espaços em branco
+  const isValid = validator.isValid(creditCardNumber);
+  const alertElement = document.getElementById('alert');
+  const invalidOptionsElement = document.getElementById('invalidOptions');
+  if (isValid) {
+    alertElement.textContent = 'Número de cartão válido!';
+    alertElement.classList.remove('alert-error');
+    alertElement.classList.add('alert-success');
+    invalidOptionsElement.style.display = 'none';
+  } else {
+    alertElement.textContent = 'Número de cartão inválido! Por favor, verifique o número digitado e tente novamente.';
+    alertElement.classList.remove('alert-success');
+    alertElement.classList.add('alert-error');
+    invalidOptionsElement.style.display = 'block';
+  }
+  alertElement.style.display = 'block';
+  setTimeout(() => {
+    alertElement.style.display = 'none';
+  }, 5000);
+  }
+  function tentarNovamente() {
+       console.log('Tentando novamente...');
+  }
+  
+  function assistirVideo() {
+       window.location.href = '/moon.mp4';
+  }
+
+
+alertElement.style.display = 'block';
+setTimeout(() => {
+  alertElement.style.display = 'none';
+}, 5000);
+
--- src/validator.js
+++ src/validator.js
@@ -1,34 +1,44 @@
 const validator = {
-
-    //Função para validar o número do cartão de crédito usando o Algoritimo de Luhn
-    isValid: function (creditCardNumber) {
-  
-    //Remover espaços em branco do número do cartão
+  //Função para validar o número do cartão de crédito usando o Algoritmo de Luhn
+  isValid: function (creditCardNumber) {
+    // Remover espaços em branco do número do cartão
     const cleanedNumber = creditCardNumber.replace(/\s/g, '');
 
-    //Converter o número de cartão em um array de dígitos
+    // Converter o número de cartão em um array de dígitos
     const digitsArray = Array.from(cleanedNumber).map(Number);
 
-    //Inverter o array de dígitos
+    // Inverter o array de dígitos
     digitsArray.reverse();
 
-    //Aplicar o Algoritimo de Luhn
-      let sum = 0;
-        for (let i = 0; i < digitsArray.length; i++) {
-          if (i % 2 !== 0) {
-            let doubleDigits = digitsArray[i] * 2;
-            if (doubleDigits > 9) {
-              doubleDigits -= 9;
-            }
+    // Aplicar o Algoritmo de Luhn
+    let sum = 0;
+    for (let i = 0; i < digitsArray.length; i++) {
+      if (i % 2 !== 0) {
+        let doubleDigits = digitsArray[i] * 2;
+        if (doubleDigits > 9) {
+          doubleDigits -= 9;
+        }
         sum += doubleDigits;
-        } else {
-        sum +=digitsArray[i];
+      } else {
+        sum += digitsArray[i];
       }
-  }
+    }
 
-   //Verificar se o resultado da soma é um múltiplo de 10
-  return sum % 10 === 0;
- },
+    // Verificar se o resultado da soma é um múltiplo de 10
+    return sum % 10 === 0;
+  },
+  maskify: function (creditCardNumber) {
+    if (creditCardNumber.length <= 4){
+      return creditCardNumber;
+    }
+    const maskedString = '#'.repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
+    return maskedString;
+  },
+  mascararNumeroCartao: function (input) {
+    const creditCardNumber = input.value.replace(/\s/g, ''); // Remover espaços em branco
+    const maskedNumber = this.maskify(creditCardNumber); // Aplicar a função maskify
+    input.value = maskedNumber; // Atualizar o valor do campo de entrada
+  },
 };
 
 export default validator;
