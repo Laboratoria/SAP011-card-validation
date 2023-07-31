6ae12db44fff484467313267fa62711b0c7fbd7a
Leticia Bezerra <leticia21bezerradearaujo@gmail.com>
2023-07-31 18:49:56 -0300

    FUNCIONOOOOOU!

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import validator from './validator.js';
 
 const submit = document.getElementById("btn-submit");
 
-const numero = document.getElementById("card").value;
-
 submit.addEventListener('click', function (e) {
   e.preventDefault();
+  const cardNumber = document.getElementById("card").value;
+  document.getElementById("card").value = validator.maskify(cardNumber);
+  if(validator.isValid(cardNumber)) {
+    document.getElementById("full-box").innerHTML = `
+    <header>
+        <h2>Cartao validado com sucesso!</h2>
+        <br>
+    </header>
+    <!-- Criar um cartao aqui com os 4U numeros visivel e talvez a bandeira aqui(se der tempo) -->
+        <br>
+        <input type="submit" id="btn-submit" value="Validar novamente">`
 
-  
-  if(validator.isValid(numero)) {
-    document.getElementById("register-form").innerHTML = `
-                    <header>
-                        <h2>Cartao validado com sucesso!</h2>
-                        <br>
-                    </header>
-                        <br>
-                        <input type="submit" id="btn-submit" value="Validar novamente">`
+    
   } else {
 
     alert("Por gentileza, verifique o numero informado e tente novamente.");
--- src/validator.js
+++ src/validator.js
@@ -4,51 +4,47 @@ const validator = {
 };
 export default validator;
 
-const numero = document.getElementById("card").value;
-const card = numero;
-const func = num => Number(num);
-const intArr = Array.from(String(card), func);
-
-console.log(intArr);
-
-function isValid(intArr) {
-  if(intArr.length >= 12 ){
-    
-    let sum = 0
-    for (let i = 0; i < intArr.length; i++){
-      let digit = intArr[i];
-      if (intArr[i] % 2 === 0){
-        digit*=2;
-        if(digit > 9){
-          digit -= 9;
+function isValid(cardNumber) {
+  let sum = 0
+  const newArray = []
+  if (cardNumber.length > 10) {
+    for (let i = 0; i < cardNumber.length; i++) {
+      let digit = parseInt(cardNumber[i]);
+      if (i % 2 === 1) {
+        digit *= 2;
+        if (digit > 9) {
+          const digitDezena = parseInt(digit / 10);
+          const digitUnidade = digit % 10;
+          const numberValidate = digitDezena + digitUnidade;
+          newArray[i] = numberValidate;
+        } else {
+          newArray[i] = digit;
         }
-        sum += digit;
+      }else{
+        newArray[i] = digit;
       }
+      sum += newArray[i];
     }
-    return sum % 10 === 0; 
-    
-  } else {
-    return false;
-  }
-  
-  function maskify(arrInt) {
-    if (arrInt.length <= 5) {
-      return str;
-    }
-  
-    const lastFourCard = arrInt.substr(arrInt.length, - 4);
-    const maskedChars = '*'.repeat(arrInt.length - 12);
-    const maskedString = maskedChars + lastFourCard;
-  
-    return maskedString;
-  }
+
+  }  
+  return cardNumber.length > 10 && sum % 10 === 0;
   
 
 }
- 
+//https://www.google.com/search?q=como+usar+funcao+maskify+javascript+no+javascript&rlz=1C1GCEA_enBR1020BR1020&biw=1366&bih=625&tbm=vid&sxsrf=AB5stBgk-9pUqo6SL8HVv7i_KMQkZ26SVA%3A1690815030866&ei=NsrHZPq5NOOz5OUPw4qVuA0&ved=0ahUKEwj6rK7qmLmAAxXjGbkGHUNFBdcQ4dUDCA0&uact=5&oq=como+usar+funcao+maskify+javascript+no+javascript&gs_lp=Eg1nd3Mtd2l6LXZpZGVvIjFjb21vIHVzYXIgZnVuY2FvIG1hc2tpZnkgamF2YXNjcmlwdCBubyBqYXZhc2NyaXB0MgUQABiiBDIFEAAYogRI2hdQrQ5YtRNwAHgAkAEAmAGaAaAB3ASqAQMxLjS4AQPIAQD4AQHCAgQQIxgniAYB&sclient=gws-wiz-video#fpstate=ive&vld=cid:15a7e39e,vid:r-8isv_TnVA
+function maskify(cardNumber) {
+  if (cardNumber.length <= 4) {
+    return cardNumber;
+  }
 
-  
- 
+  const lastFourCard = cardNumber.slice(cardNumber.length - 4, cardNumber.length);
+  const maskedChars = "#".repeat(cardNumber.length - 4);
+       
+  return maskedChars + lastFourCard;
+}
+
+
+
 
 
 // function security(valor) {
