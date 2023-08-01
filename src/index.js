5ebd4b6b5851f0914f48563c94b87f7e9a0890e3
Dahbarbara <dahbarbara_@hotmail.com>
2023-08-01 14:10:54 -0300

    Projeto Validator finalizado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-import validator from './validator.js';
+import validator from "./validator.js"
 
-console.log(validator);
+function isValid() {
+  // Obtém o valor inserido no campo de número do cartão
+  const numeroCartao = document.getElementsByName('number')[0].value;
+
+  // Obtém o valor inserido no campo de nome do titular do cartão
+  const nomeTitular = document.getElementsByName('NOME')[0].value;
+
+  // Cria uma string vazia para armazenar a mensagem de validação do cartão
+  let mensagem = '';
+
+  // Verifica se o cartão é válido usando a função 'isValid' do módulo 'validator'
+  if (validator.isValid(numeroCartao)) {
+    // Se o cartão for válido, mascara o número do cartão e atualiza a mensagem
+    const maskedCartao = validator.maskify(numeroCartao)
+    mensagem = `O cartão ${maskedCartao} é válido. Titular: ${nomeTitular}.`;
+  } else {
+    // Se o cartão for inválido, atualiza a mensagem de erro
+    mensagem = 'O cartão é inválido.';
+  }
+
+  // Obtém o elemento que representa o modal
+  const modal = document.getElementById('modal');
+
+  // Obtém o elemento onde será exibido o resultado da validação do cartão
+  const resultadoDiv = document.getElementById('resultado');
+
+  // Exibe a mensagem de validação no elemento de resultado
+  resultadoDiv.innerText = mensagem;
+
+  // Exibe o modal tornando-o visível
+  modal.style.display = 'block';
+
+  // Obtém o elemento que representa o conteúdo do modal
+  const modalContent = document.getElementById('modal-content');
+
+  // Aplica a animação de entrada do modal
+  modalContent.style.animation = 'zoomIn 0.3s';
+}
+
+function fecharModal() {
+  // Obtém o elemento que representa o modal
+  const modal = document.getElementById('modal');
+
+  // Obtém o elemento que representa o conteúdo do modal
+  const modalContent = document.getElementById('modal-content');
+
+  // Aplica a animação de saída do modal
+  modalContent.style.animation = 'zoomOut 0.3s';
+
+  // Após um breve intervalo (300ms), esconde completamente o modal
+  setTimeout(() => {
+    modal.style.display = 'none';
+
+    // Limpa o conteúdo do resultado quando o modal é fechado
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerText = '';
+  }, 300);
+}
+
+
+// Adiciona um evento de clique ao botão "Verificar" para chamar a função 'isValid'
+document.getElementById('botaov').addEventListener('click', isValid);
+
+// Adiciona um evento de clique ao botão "Fechar" do modal para chamar a função 'fecharModal'
+document.getElementById('fecharModal').addEventListener('click', fecharModal);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
+// Define um objeto chamado 'validator' que contém duas funções: 'isValid' e 'maskify'
 const validator = {
-  // ...
-};
+  // Função 'isValid': recebe um número de cartão como entrada e verifica se é válido
+  isValid : function (numberCard) {
+    // Inicializa a variável 'soma' com 0, será usada para calcular a soma dos dígitos
+    let soma = 0;
+    // Inicializa a variável 'dobro' como false, será usada para alternar entre dobrar ou não o valor do dígito
+    let dobro = false;
 
+    // Loop para percorrer os dígitos do cartão, começando do último e indo até o primeiro
+    for (let i = numberCard.length - 1; i >= 0; i--) {
+      // Converte o caractere para um número inteiro
+      let digit = parseInt(numberCard.charAt(i), 10);
+
+      // Verifica se o dígito deve ser dobrado
+      if (dobro) {
+        digit *= 2;
+        // Se o dígito dobrado for maior que 9, subtrai 9 para garantir que seja menor ou igual a 9
+        if (digit > 9) digit -= 9;
+      }
+
+      // Adiciona o dígito à soma total
+      soma += digit;
+      // Alterna o valor de 'dobro' para o próximo dígito
+      dobro = !dobro;
+    }
+
+    // Retorna true se a soma for divisível por 10 (ou seja, se o cartão é válido), senão retorna false
+    return soma % 10 === 0;
+  },
+
+  // Função 'maskify': recebe um número de cartão como entrada e mascara os dígitos, exceto os últimos 4
+  maskify : function (numberCard) {
+    // Se o número de cartão tiver 4 dígitos ou menos, não é necessário mascarar, então retorna o número original
+    if (numberCard.length <= 4) return numberCard;
+
+    // Obtém os últimos 4 dígitos do número do cartão
+    const ultimos = numberCard.slice(-4);
+
+    // Obtém os dígitos restantes do número do cartão e substitui todos os dígitos e letras por '#'
+    const esconder = numberCard.slice(0, -4).replace(/\d|\w/g, '#');
+
+    // Combina os dígitos mascarados com os últimos 4 dígitos originais e retorna o resultado
+    const Ultimosesconder = esconder + ultimos;
+
+    return Ultimosesconder;
+  }
+}
+
+// Exporta o objeto 'validator' para ser utilizado em outros arquivos
 export default validator;
